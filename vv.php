493d0ff2d657eb71027434ba06907bd36087bdf0
lisek18 <98351097+lisek18@users.noreply.github.com>
2022-11-06 20:56:43 +0100

    Add files via upload

---
+++ vv.php
@@ -0,0 +1,17 @@
+<?php
+$ff=$_GET["Nazwisko"];
+$vv=$_GET["Imie"];
+$bb=$_GET["Stanowisko"];
+$hh=$_GET["Tel"];
+
+
+echo 'Hello ' . $ff . ' '. $vv. ' '. $bb. ' '. $hh ;
+
+$con = new mysqli("127.0.0.1","root","","baza");
+
+$xd=$_GET["hhgg"];
+
+$q="DELETE FROM tab WHERE id='$xd'";
+ $wynik=$con->query($q)
+
+?>
